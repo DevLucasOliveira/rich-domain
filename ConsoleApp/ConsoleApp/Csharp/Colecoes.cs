536bf2952a8d8155a9c706561150f53c40b2234f
Lucas Oliveira <lucasoliveira.si@outlook.com>
2020-05-21 11:54:58 -0300

    Finalizando primeiro módulo do curso

---
+++ ConsoleApp/ConsoleApp/Csharp/Colecoes.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp.Curso
+{
+    class Colecoes
+    {
+        //static void Main(string[] args)
+        //{
+        //    var arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        //    var nomes = new string[]
+        //    {
+        //        "ana",
+        //        "joão",
+        //        "pedro",
+        //        "paulo",
+        //        "maria",
+        //        "beatriz"
+        //    };
+
+        //    foreach (var nome in nomes)
+        //    {
+        //        Console.WriteLine(nome);
+        //    }
+
+        //    var cidades = new List<string>
+        //    {
+        //        "São Paulo",
+        //        "Natal",
+        //        "Rio de Janeiro",
+        //        "Brasília"
+        //    };
+
+        //    cidades.Add("Curitiba");
+        //    cidades.Remove("Natal");
+
+        //    var filtradas = from c in cidades
+        //                    where c.StartsWith("S")
+        //                    select c;
+
+        //    var lambda = cidades.Where(x => x.StartsWith("N"));
+
+        //    foreach (var cidade in filtradas)
+        //    {
+        //        Console.WriteLine(cidade);
+        //    }
+
+        //    Console.Read();
+        //}
+    }
+}
---
+++ ConsoleApp/ConsoleApp/Csharp/TratamentoExcecao.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp.Curso
+{
+    class TratamentoExcecao
+    {
+        //public static void Main(string[] args)
+        //{
+
+        //    var nome = "Lucas";
+        //    var idade = 0;
+        //    var maiorDeIdade = idade > 18;
+
+        //    try
+        //    {
+        //        if (idade == 0)
+        //            throw new Exception("Idade Inválida");
+
+        //        if (maiorDeIdade)
+        //            Console.WriteLine(nome + " é maior de idade ");
+        //        else
+        //            Console.WriteLine(nome + " Não é maior de idade");
+
+        //        Console.WriteLine(nome + " tem " + idade + " anos");
+
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Console.WriteLine("Ocorreu um erro no sistema: " + e.Message);
+        //    }
+
+        //    Console.ReadLine();
+        //}
+    }
+}
